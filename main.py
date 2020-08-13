113fe10cbbf7f6107a0e021e8f4bd71fa022c3f4
jusola <juulain@gmail.com>
2020-08-13 19:27:06 +0300

    initial implementation

--- main.py
+++ main.py
@@ -1,2 +1,36 @@
-import hosts
+import configparser
+from hosts import hosts
+import wifi
+import time
+import os
 
+
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+if os.name == 'nt':
+    hosts_path = os.path.join(os.environ['SYSTEMROOT'], 'system32/drivers/etc/hosts')
+elif os.name == 'posix':
+    hosts_path = '/etc/hosts'
+else:
+    raise Exception('Unsupported OS: %s' % os.name)
+
+print(hosts_path)
+
+iface = wifi.Wifi()
+hosts_manager = hosts.Hosts(hosts_path)
+
+while True:
+    curSSID = iface.getSSID()
+    if (not curSSID is None):
+        if(curSSID == config['hostsmanager']['SSID']):
+            hosts_manager.set_one(config['hostsmanager']['hostname'], config['hostsmanager']['target'])
+            hosts_manager.write(hosts_path)
+        else:
+            hosts_manager.remove_one(config['hostsmanager']['host'], False)
+            hosts_manager.write(hosts_path)
+    else:
+        hosts_manager.remove_one(config['hostsmanager']['host'], False)
+        hosts_manager.write(hosts_path)
+
+    time.sleep(30)
